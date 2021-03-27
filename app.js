deec968ddc988d25075bfff5c9880a04bfe293f5
yarishred <yfruiz9@gmail.com>
2021-03-27 12:01:30 -0500

    Funcionalidad principal del simulador acabada, agrega SVG y agrega color a cada uno de los patrones

--- app.js
+++ app.js
@@ -2,20 +2,29 @@
 const colorItems = document.querySelectorAll(".color");
 const forms = document.querySelectorAll(".form");
 const mainView = document.querySelector(".item-main-view");
+const pickColor = document.querySelector("h4");
+const resetButton = document.querySelector('.reset-btn')
+const myItems = document.querySelectorAll('.item')
+const detailedForm = document.querySelectorAll('.detailedForm')
 // Variables
 let activeColor;
-let myStyle
+let myStyle;
 
 //Funciones
 const selectColor = (element) => {
   const colorElement = getComputedStyle(element.target);
   const color = colorElement.backgroundColor;
   activeColor = color;
+  pickColor.textContent = color;
 };
 
-const changeColorElement = (event)=>{
-    const changeColor = event.target
-    changeColor.style.backgroundColor = activeColor
+const changeColorElement = (event) => {
+  const changeColor = event.target;
+  changeColor.style.backgroundColor = activeColor;
+};
+
+const resetElement = ()=>{
+  mainView.innerHTML=''
 }
 
 //Eventos
@@ -23,21 +32,23 @@ colorItems.forEach((colorItem) =>
   colorItem.addEventListener("click", selectColor)
 );
 
-forms.forEach((element, index) => {
-  element.addEventListener("click", function (formStyle) {
+myItems.forEach((element, index) => {
+  element.addEventListener("click", ()=> {
     element.classList.add("active");
-    forms.forEach((elmt, indx) => {
+    myItems.forEach((elmt, indx) => {
       if (index !== indx) {
         elmt.classList.remove("active");
         mainView.innerHTML = "";
       }
     });
-    myStyle = document.createElement('div')
-    myStyle.classList.add('detailedForm')
-    myStyle.textContent = formStyle.target.innerText
-
-    myStyle.addEventListener('click', changeColorElement)
-    mainView.append(myStyle);
+   let newElement = element.cloneNode(true)
+   newElement.style.transform = `scale(2.5)`
+   newElement.addEventListener('click', (e)=>{
+    e.target.style.fill = activeColor
+   })
+    mainView.append(newElement);
   });
 });
+
+resetButton.addEventListener('click', resetElement)
 
